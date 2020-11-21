da42a1ad1ecbf3e06ac3a6fb97f3ad88c2a79ec1
Márcio Fetter <fetter.marcio@gmail.com>
2020-11-21 15:24:13 -0300

    adcionado tela carrinho e menu item

--- app/src/main/java/br/fetter/rulethemall/activities/HomeListActivity.kt
+++ app/src/main/java/br/fetter/rulethemall/activities/HomeListActivity.kt
@@ -1,8 +1,11 @@
 package br.fetter.rulethemall.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import br.fetter.rulethemall.R
 import br.fetter.rulethemall.model.Product
@@ -23,12 +26,27 @@ class HomeListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.title = "Lista de produtos"
         setContentView(R.layout.home_list)
     }
 
     override fun onResume() {
         super.onResume()
         getProducts()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.cart) {
+            val intent = Intent(this, CartActivity::class.java)
+            startActivity(intent)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun getProducts() {
