2c4e99ec5d28d93f5bd2bbe9cb62ff8ae914a998
Márcio Fetter <fetter.marcio@gmail.com>
2020-11-21 18:07:14 -0300

    removido classes não utilizadas

--- app/src/main/java/br/fetter/rulethemall/service/ProductService.kt
+++ app/src/main/java/br/fetter/rulethemall/service/ProductService.kt
@@ -1,15 +1,23 @@
 package br.fetter.rulethemall.service
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
+import androidx.room.*
 import br.fetter.rulethemall.model.ProductCart
-import retrofit2.Call
-import retrofit2.http.GET
 
 @Dao
 interface ProductService {
 
     @Insert
     fun save(product: ProductCart)
+
+    @Query(value = "select * from ProductCart")
+    fun getAll(): List<ProductCart>
+
+    @Delete
+    fun delete(note: ProductCart)
+
+    @Query("SELECT * FROM ProductCart WHERE idProduto=:id")
+    fun loadSingle(id: Int): ProductCart
+
+    @Update
+    fun update(note: ProductCart)
 }
